a4c041d7688519002f8bf34dbc94f7b56f669797
varun sachdeva <sachdevavarun7@gmai.com>
2021-07-18 00:01:22 +0530

    fix airport npc, artifacts.json and rule card

--- src/components/AirportNPC/AirportNPC.jsx
+++ src/components/AirportNPC/AirportNPC.jsx
@@ -7,20 +7,37 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import * as artifactData from "../Data/artifacts.json";
 import { getDistance } from "geolib";
+import loDash from "lodash";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import IconButton from "@material-ui/core/IconButton";
+import clsx from "clsx";
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		maxWidth: 250,
+		maxWidth: 245,
 		float: "right",
-		"& > *": {
-			margin: theme.spacing(1),
-			width: "25ch",
-		},
+		padding: 10,
+	},
+	expand: {
+		transform: "rotate(0deg)",
+		marginLeft: "auto",
+		transition: theme.transitions.create("transform", {
+			duration: theme.transitions.duration.shortest,
+		}),
+	},
+	expandOpen: {
+		transform: "rotate(180deg)",
+	},
+	form: {
+		padding: 10,
+		width: 200,
 	},
 }));
 
 function AirportNPC() {
 	const classes = useStyles();
+	const [expanded, setExpanded] = useState(false);
 	const [flightData, setFlightData] = useState({
 		departure: "",
 		arrival: "",
@@ -43,7 +60,7 @@ function AirportNPC() {
 	const handleChange = event => {
 		const { name, value } = event.target;
 		setFlightData(prevValues => {
-			return { ...prevValues, [name]: value };
+			return { ...prevValues, [name]: loDash.capitalize(value) };
 		});
 	};
 
@@ -58,19 +75,52 @@ function AirportNPC() {
 		});
 	};
 
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	};
+
 	return (
 		<Card className={classes.root}>
-			<CardHeader title="Airport" />
-			<CardContent>
-				<form className={classes.root}>
-					<TextField name="departure" placeholder="Departure" variant="outlined" onChange={handleChange} />
-					<TextField name="arrival" placeholder="Arrival" variant="outlined" onChange={handleChange} />
-					<Button variant="contained" color="primary" onClick={handleClick}>
-						Calculate
-					</Button>
-					<TextField name="fare" placeholder="Fare" variant="outlined" value={fare} />
-				</form>
-			</CardContent>
+			<CardHeader
+				title="Airport"
+				action={
+					<IconButton
+						className={clsx(classes.expand, {
+							[classes.expandOpen]: expanded,
+						})}
+						onClick={handleExpandClick}
+						aria-expanded={expanded}
+						aria-label="show more">
+						<ExpandMoreIcon />
+					</IconButton>
+				}
+			/>
+			<Collapse in={expanded} timeout="auto" unmountOnExit>
+				<CardContent>
+					<form className={classes.root} autoComplete="off">
+						<TextField
+							className={classes.form}
+							size="small"
+							name="departure"
+							placeholder="Departure"
+							variant="outlined"
+							onChange={handleChange}
+						/>
+						<TextField
+							className={classes.form}
+							size="small"
+							name="arrival"
+							placeholder="Arrival"
+							variant="outlined"
+							onChange={handleChange}
+						/>
+						<Button className={classes.form} variant="contained" color="primary" onClick={handleClick}>
+							Calculate
+						</Button>
+						<TextField className={classes.form} size="small" name="fare" placeholder="Fare" variant="outlined" value={fare} />
+					</form>
+				</CardContent>
+			</Collapse>
 		</Card>
 	);
 }
